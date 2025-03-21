833bb230b27897e06bb7628569fc800733518dd5
jisan-05 <zishantanvir05@gmail.com>
2025-03-21 10:37:55 +0600

    h swiper

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,9 +1,12 @@
+
+import Category from "../Category/Category.jsx";
 import Banner from "./Banner/Banner";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <Category></Category>
     </div>
   );
 };
