1b92bc85ba99669993f4076510fa81845ff18ad2
jisan-05 <zishantanvir05@gmail.com>
2025-03-22 14:35:17 +0600

    implement dynamic heading and subheading

---
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import SectionTitle from '../../../Components/SectionTitle/SectionTitle';
+
+const PopularMenu = () => {
+    return (
+        <section>
+            <SectionTitle 
+            heading="from our menu"
+            subHeading="Popular Items"
+            >
+                
+            </SectionTitle>
+        </section>
+    );
+};
+
+export default PopularMenu;
