dc0b8fc58ec77051f6e92caac614c40d0647c4f1
Tanvir <zishantanvir05@gmail.com>
2025-03-25 15:28:15 +0600

    implement validation from react hook from and signup complete using react hook form

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,6 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useForm } from "react-hook-form";
+import { AuthContext } from "../../providers/AuthProvider";
 
 const SignUp = () => {
+    const {createUser} = useContext(AuthContext)
+
+    const {
+        register,
+        handleSubmit,
+
+        formState: { errors },
+    } = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data);
+        createUser(data.email,data.password)
+        .then(result =>{
+            const loggedUser = result.user;
+            console.log(loggedUser)
+        })
+    };
+
+    // console.log(watch("example"))
+
     return (
         <div className="hero bg-base-200 min-h-screen">
             <div className="hero-content flex-col lg:flex-row-reverse">
@@ -13,39 +35,75 @@ const SignUp = () => {
                     </p>
                 </div>
                 <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-                    <div className="card-body">
+                    <form
+                        onSubmit={handleSubmit(onSubmit)}
+                        className="card-body"
+                    >
                         <fieldset className="fieldset">
                             <label className="fieldset-label">Name</label>
                             <input
                                 type="text"
                                 name="name"
+                                {...register("name", { required: true })}
                                 className="input"
                                 placeholder="Enter Name"
                             />
+                            {errors.name && (
+                                <span className="text-red-500">
+                                    Name is required
+                                </span>
+                            )}
                             <label className="fieldset-label">Email</label>
                             <input
                                 type="email"
                                 name="email"
+                                {...register("email", { required: true })}
                                 className="input"
                                 placeholder="Email"
                             />
+                            {errors.email && (
+                                <span className="text-red-500">
+                                    email is required
+                                </span>
+                            )}
                             <label className="fieldset-label">Password</label>
                             <input
                                 type="password"
                                 name="password"
+                                {...register("password", {
+                                    required: "Password is required",
+                                    minLength: {
+                                        value: 8,
+                                        message:
+                                            "Password must be at least 8 characters long",
+                                    },
+                                    maxLength: {
+                                        value: 20,
+                                        message:
+                                            "Password must be at most 20 characters long",
+                                    },
+                                    pattern:
+                                        /(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$&*]).{8,20}/,
+                                })}
                                 className="input"
                                 placeholder="Password"
                             />
+                            {errors.password && (
+                                <p className="text-red-500">
+                                    {errors.password.message}
+                                </p>
+                            )}
+                            {errors.password?.type === "pattern" && (
+                                <p className="text-red-500">Password must be 1 uppercase 1 lowercase and 1 digit and 1 special characters</p>
+                            )}
                             <div>
                                 <a className="link link-hover">
                                     Forgot password?
                                 </a>
                             </div>
-                            <button className="btn btn-neutral mt-4">
-                                Login
-                            </button>
+                            <input className="btn btn-neutral mt-4" type="submit" value="Sign Up" />
                         </fieldset>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
