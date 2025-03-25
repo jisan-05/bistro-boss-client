9f391f2081c413e9cb3c7b1164a115cdf9cce91a
Tanvir <zishantanvir05@gmail.com>
2025-03-25 12:14:00 +0600

    implement react-simple-captcha from react awesome components and design login page

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,26 +3,31 @@ import Main from "../Layout/Main";
 import Home from "../pages/Home/Home/Home";
 import Menu from "../pages/Menu/Menu/Menu";
 import Order from "../pages/Order/Order/Order";
+import Login from "../pages/Login/Login";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Main></Main>,
-    children:[
-      {
-        path:'/',
-        element:<Home></Home>
-      },
-      {
-        path:'menu',
-        element:<Menu></Menu>
-      },
-      {
-        path:'order/:category',
-        element:<Order></Order>
-      }
-    ]
-  },
+    {
+        path: "/",
+        element: <Main></Main>,
+        children: [
+            {
+                path: "/",
+                element: <Home></Home>,
+            },
+            {
+                path: "menu",
+                element: <Menu></Menu>,
+            },
+            {
+                path: "order/:category",
+                element: <Order></Order>,
+            },
+            {
+              path:"login",
+              element:<Login></Login>
+            },
+        ],
+    },
 ]);
 
 export default router;
