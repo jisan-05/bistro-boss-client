1cec13051c538af1e55af0bbb4e8724627a19160
Tanvir <zishantanvir05@gmail.com>
2025-03-30 15:18:38 +0600

    complete set api for upload image in imageBB

--- src/pages/Dashboard/AddItems/AddItems.jsx
+++ src/pages/Dashboard/AddItems/AddItems.jsx
@@ -3,10 +3,24 @@ import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 import ReactDOM from "react-dom";
 import { useForm } from "react-hook-form";
 import { FaUtensils } from "react-icons/fa";
+import useAxiosPublic from "../../../hooks/useAxiosPublic";
 
+const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`
 const AddItems = () => {
     const { register, handleSubmit } = useForm();
-    const onSubmit = (data) => console.log(data);
+    const axiosPublic = useAxiosPublic()
+    const onSubmit =async (data) => {
+      console.log(data)
+      // image upload to imgbb and thn get an url
+      const imageFile = {image: data.image[0]}
+      const res = await axiosPublic.post(image_hosting_api, imageFile,{
+        headers:{
+          'content-type': 'multipart/form-data'
+        }
+      })
+      console.log(res.data)
+    };
     return (
         <div>
             <SectionTitle
