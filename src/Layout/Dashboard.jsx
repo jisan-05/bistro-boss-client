84c03188fd946616bd4d042a49b7262b6252deba
Tanvir <zishantanvir05@gmail.com>
2025-04-09 20:01:10 +0600

    complete 70.3

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -63,7 +63,7 @@ const Dashboard = () => {
                                 </NavLink>
                             </li>
                             <li>
-                                <NavLink to="/dashboard/reservation">
+                                <NavLink to="/dashboard/payment">
                                     <FaCalendar></FaCalendar>Reservation
                                 </NavLink>
                             </li>
